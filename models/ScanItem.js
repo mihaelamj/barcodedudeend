02e318ba71ad20cba8e6edd00f78c1be0c9b5bfe
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2016-02-28 19:34:53 +0100

    added items to import

--- models/ScanItem.js
+++ models/ScanItem.js
@@ -6,6 +6,10 @@ var ScanItemModel = new Schema({
     format: {type: String, required: true},
     author: {type: String, required: false},
     identifier: {type: String, required: true},
+    device: {type: String, required: true},
+    ios: {type: String, required: true},
+    udid: {type: String, required: true},
+    engine: {type: String, required: true},
     date: {type: Date, default: Date.now},
     hyperlink: {type: Boolean, default:false},
     correct: {type: Boolean, default:false}
